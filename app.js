6873d000e4d7595df9bde15b413319970856391c
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-05-09 19:46:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const { url } = require("inspector");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const eMail = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: eMail,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = process.env.MAILCHIMP_URL;
+
+  const options = {
+    method: "POST",
+    auth: process.env.AUTH_KEY,
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {});
+  });
+
+  request.write(jsonData);
+
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
+});
